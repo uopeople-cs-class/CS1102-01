43e004f209f5e93fd9d744120135737896aaec15
Davisone <67480359+krush-ghod@users.noreply.github.com>
2026-05-12 08:54:43 -0400

    Saved

--- learningPracticals/src/practice.java
+++ learningPracticals/src/practice.java
@@ -1,23 +1,17 @@
 public class practice {
     public static void main(String[] args) throws Exception {
-        // initialize the variables
-        int firstDice;
-        int secondDice;
-        int totalRoll;
+        int i = 1;
 
-        // get random values and assign it to firstDice and secondDice
-        firstDice = (int)(Math.random()*6)+1;
-        secondDice =(int)(Math.random()*6)+1;
+        while (i <= 5) {
 
-        // compute the total
-        totalRoll = firstDice + secondDice;
+            if (i % 2 == 0)
 
-        // display output
-        System.out.println("Your first Dice came up as:");
-        System.out.println(firstDice);
-        System.out.println("Your second Dice came up as:");
-        System.out.println(secondDice);
-        System.out.println("Your total Dice roll is:");
-        System.out.println(totalRoll);
+                continue;
+
+            System.out.print(i + " ");
+
+            i++;
+
+        }
     }
 }
---
+++ weekFour/src/paFour.java
@@ -0,0 +1,95 @@
+/*
+    This file contains a stock price list. Using an array to return the maximum price, ...
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class paFour {
+    // in here just do the list and methods
+    public static void main(String[] args) {
+        int[] dailyPrices = {230, 200, 900, 342, 700, 433, 500, 900, 900, 900};
+        // before you can print all use a loop
+        float mean = calculateAveragePrice(dailyPrices);
+        System.out.println(mean);
+        double max = findMaximumPrice(dailyPrices);
+        System.out.println(max);
+        int occurance = countOccurance(dailyPrices);
+        System.out.println(occurance);
+        ArrayList<Integer> dailyPriceList =
+            new ArrayList<>(Arrays.asList(230, 200, 900, 342, 700, 433, 500, 900, 900, 900));
+        ArrayList<Integer> cumulativePriceList = new ArrayList<>();
+        cumulativePriceList = computeCumulativeSum(dailyPriceList);
+        System.out.println(cumulativePriceList);
+    }
+
+
+    // calculate average price
+    static float  calculateAveragePrice(int[] dps){
+        // what this function does is that, it takes the list and calculate avg
+        int counter = 0;
+        int total = 0;
+        for (int i : dps) {
+            total += i;
+            counter++;
+        }
+        float  avg = total/counter;
+        return avg;
+    }
+
+    // find the maximum number
+    static double findMaximumPrice(int[] dps){
+        int maxNumber = 0;
+        for (int x : dps) {
+            if(x>maxNumber){
+                maxNumber = x;
+            }
+        }
+        return maxNumber;
+    }
+
+    // this method gets an input value from the user and count the number of time the values is in the list
+    static int countOccurance(int[] dps) {
+        // set the counted to zero
+        int count = 0;
+        // turn on scanner for inputs
+        Scanner input = new Scanner(System.in);
+        // takes the input from the user
+        while (true) { 
+            try {
+                // get the input
+                System.out.println("Enter the price you want to count it occurance:");
+                int value = Integer.parseInt(input.nextLine());
+                for (int i : dps) {
+                    if (i == value){
+                        // update count
+                        count++;
+                    }
+                }
+                return count;
+            }
+            catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    // cumulative sum
+    static ArrayList<Integer> computeCumulativeSum(ArrayList<Integer> dpsl){
+        ArrayList<Integer> cumList = new ArrayList<>();
+        int cum = 0;
+        for (int i = 0; i < dpsl.size(); i++) {
+            if(i > 0){
+                cum = cum + dpsl.get(i);
+                cumList.add(cum);
+            }
+            if(i == 0){
+                cum = dpsl.get(i);
+                cumList.add(cum);
+            }
+        }
+        return cumList;
+    }
+
+}
